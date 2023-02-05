86198cc294937dac0bd47938566d80d176b68046
Eduardo Bordin <99068107+EduardoSBordin@users.noreply.github.com>
2023-02-04 21:10:17 -0300

    up.2.5

--- main_content/scriptProfile.js
+++ main_content/scriptProfile.js
@@ -5,9 +5,13 @@ btnDarkMode.addEventListener('click', () => {
     document.querySelector('.infBlack1').classList.toggle('activeBody');
     document.querySelector('.infBlack2').classList.toggle('activeBody');
     document.querySelector('.infBlack3').classList.toggle('activeBody');
+    document.querySelector('.infBlack4').classList.toggle('activeBody');
+    document.querySelector('.infBlack5').classList.toggle('activeBody');
     document.querySelector('#navBar').classList.toggle('activeBody');
     document.querySelector('#h1').classList.toggle('activeBody');
     document.querySelector('#h1_2').classList.toggle('activeBody');
     document.querySelector('#h1_3').classList.toggle('activeBody');
+    document.querySelector('#h1_4').classList.toggle('activeBody');
+    document.querySelector('#h1_5').classList.toggle('activeBody');
     document.querySelector('#iconDarkMode').classList.toggle('activeBody');
 });
