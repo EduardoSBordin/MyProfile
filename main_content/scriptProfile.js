7fba22e1413e991498723f34a259a04423af7d95
Eduardo Bordin <99068107+EduardoSBordin@users.noreply.github.com>
2023-02-05 01:54:35 -0300

    update-about-2.3

--- main_content/scriptProfile.js
+++ main_content/scriptProfile.js
@@ -15,3 +15,22 @@ btnDarkMode.addEventListener('click', () => {
     document.querySelector('#h1_5').classList.toggle('activeBody');
     document.querySelector('#iconDarkMode').classList.toggle('activeBody');
 });
+
+document.querySelector('#modalDiv').style.marginTop = '-200px';
+let clickId = 1;
+document.querySelector('.btnProjects').addEventListener('click', () =>{
+
+    // document.querySelector('#modalDiv').style.display = 'block';
+    clickId++;
+
+    if(clickId <= 1){
+        //document.querySelector('#modalDiv').style.display = 'block';
+        document.querySelector('#modalDiv').style.marginTop = '-200px';
+        document.querySelector('#modalDiv').style.transition = '0.5s';
+    }else{
+        clickId = 0;
+        //document.querySelector('#modalDiv').style.display = 'none';
+        document.querySelector('#modalDiv').style.marginTop = '200px';
+        document.querySelector('#modalDiv').style.marginTop = '0px';
+    }
+});
