6f000cd5934ad2c9d9cd15355c344f79806e7364
Eduardo Bordin <99068107+EduardoSBordin@users.noreply.github.com>
2023-02-05 03:37:11 -0300

    update-about-2.4

--- main_content/scriptProfile.js
+++ main_content/scriptProfile.js
@@ -16,21 +16,20 @@ btnDarkMode.addEventListener('click', () => {
     document.querySelector('#iconDarkMode').classList.toggle('activeBody');
 });
 
-document.querySelector('#modalDiv').style.marginTop = '-200px';
-let clickId = 1;
+//document.querySelector('#modalDiv').style.marginTop = '-200px';
+let clickId = 0;
 document.querySelector('.btnProjects').addEventListener('click', () =>{
 
     // document.querySelector('#modalDiv').style.display = 'block';
     clickId++;
 
     if(clickId <= 1){
-        //document.querySelector('#modalDiv').style.display = 'block';
-        document.querySelector('#modalDiv').style.marginTop = '-200px';
-        document.querySelector('#modalDiv').style.transition = '0.5s';
+        document.querySelector('#modalDiv').style.display = 'block';
+        //document.querySelector('#modalDiv').style.marginTop = '-200px';
+        //document.querySelector('#modalDiv').style.transition = '0.5s';
     }else{
         clickId = 0;
-        //document.querySelector('#modalDiv').style.display = 'none';
-        document.querySelector('#modalDiv').style.marginTop = '200px';
-        document.querySelector('#modalDiv').style.marginTop = '0px';
+        document.querySelector('#modalDiv').style.display = 'none';
+        //document.querySelector('#modalDiv').style.marginTop = '0px';
     }
 });
