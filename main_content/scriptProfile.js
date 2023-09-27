f2ccfe51e2eda62b486f6ed49b64449949a443fb
Eduardo Bordin <dudubordin26@gmail.com>
2023-09-27 14:06:43 -0300

    NewUpdate

--- main_content/scriptProfile.js
+++ main_content/scriptProfile.js
@@ -1,35 +1,60 @@
 const btnDarkMode = document.querySelector('#btnDarkMode');
+let getB = document.querySelector('body');
 
+let clickId = 0;
 btnDarkMode.addEventListener('click', () => {
-
-    document.querySelector('.infBlack1').classList.toggle('activeBody');
-    document.querySelector('.infBlack2').classList.toggle('activeBody');
-    document.querySelector('.infBlack3').classList.toggle('activeBody');
-    document.querySelector('.infBlack4').classList.toggle('activeBody');
-    document.querySelector('.infBlack5').classList.toggle('activeBody');
-    document.querySelector('#navBar').classList.toggle('activeBody');
+    
+    // document.querySelector('.infBlack1').classList.toggle('activeBody');
+    // document.querySelector('.infBlack2').classList.toggle('activeBody');
+    // document.querySelector('.infBlack3').classList.toggle('activeBody');
+    // document.querySelector('.infBlack4').classList.toggle('activeBody');
+    // document.querySelector('.infBlack5').classList.toggle('activeBody');
+    // document.querySelector('#navBar').classList.toggle('activeBody');
     document.querySelector('#h1').classList.toggle('activeBody');
     document.querySelector('#h1_2').classList.toggle('activeBody');
     document.querySelector('#h1_3').classList.toggle('activeBody');
     document.querySelector('#h1_4').classList.toggle('activeBody');
     document.querySelector('#h1_5').classList.toggle('activeBody');
-    document.querySelector('#iconDarkMode').classList.toggle('activeBody');
+    // document.querySelector('#iconDarkMode').classList.toggle('activeBody');
+
+    //getB.classList.toggle('activeBody');
+
+    getTheme();
+    
 });
 
-//document.querySelector('#modalDiv').style.marginTop = '-200px';
-let clickId = 0;
-document.querySelector('.btnProjects').addEventListener('click', () =>{
-
-    // document.querySelector('#modalDiv').style.display = 'block';
+function getTheme(){
+    
     clickId++;
-
     if(clickId <= 1){
-        document.querySelector('#modalDiv').style.display = 'block';
-        //document.querySelector('#modalDiv').style.marginTop = '-200px';
-        //document.querySelector('#modalDiv').style.transition = '0.5s';
+        getB.style.background = 'black';
+        //console.log(getB.value);
     }else{
         clickId = 0;
-        document.querySelector('#modalDiv').style.display = 'none';
-        //document.querySelector('#modalDiv').style.marginTop = '0px';
+        getB.style.background = 'whitesmoke';
+        //console.log(getB.value);
     }
-});
+
+    localStorage.setItem('background', getB.style.background);
+    console.log(getB.style.background);
+}
+
+
+document.querySelector('body').style.background = localStorage.getItem('background');
+
+//document.querySelector('#modalDiv').style.marginTop = '-200px';
+// document.querySelector('.btnProjects').addEventListener('click', () =>{
+
+//     // document.querySelector('#modalDiv').style.display = 'block';
+//     clickId++;
+
+//     if(clickId <= 1){
+//         document.querySelector('#modalDiv').style.display = 'block';
+//         //document.querySelector('#modalDiv').style.marginTop = '-200px';
+//         //document.querySelector('#modalDiv').style.transition = '0.5s';
+//     }else{
+//         clickId = 0;
+//         document.querySelector('#modalDiv').style.display = 'none';
+//         //document.querySelector('#modalDiv').style.marginTop = '0px';
+//     }
+// });
